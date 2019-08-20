61fcc782da1676e0954ef6f969cf59a75b6dd2ab
Roman Gorelik <romang31@gmail.com>
2019-08-20 16:42:47 -0400

    testing

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  test = 'test'
 
   constructor() { }
 
